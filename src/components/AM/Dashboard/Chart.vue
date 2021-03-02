1f79ca7f565ed43e24a37cd6421d630acd833a95
enggisugiana <enggi.sugiana@gmail.com>
2021-03-02 18:51:50 +0700

    Add Chart Vuechartjs dan ChartJs untuk dashboard

---
+++ src/components/AM/Dashboard/Chart.vue
@@ -0,0 +1,57 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+    extends: Bar,
+    props: {
+      activation: {
+        type: Object,
+			  required: true
+      }
+    },
+    data () {
+      return {
+        datacollection: {
+          labels: this.activation.title,
+          datasets: [
+            {
+              backgroundColor: ["#FD0333", "#F8C310", "#43DEFA", "#43FAD0", "#05F821", "#02AC16"],
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#249EBF',
+              data: this.activation.value
+            }
+          ]
+        },
+        options: {
+          barRoundness:1,
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontColor: ["#FD0333", "#F8C310", "#43DEFA", "#43FAD0", "#05F821", "#02AC16"]
+              }
+            }]
+          },
+          legend: {
+            display: false
+          },
+          responsive: true,
+          maintainAspectRatio: true
+        }
+      }
+    },
+    mounted () {
+      this.renderChart(this.datacollection, this.options)
+    }
+}
+</script>
--- src/views/AM/Dashboard.vue
+++ src/views/AM/Dashboard.vue
@@ -13,8 +13,116 @@
         </section>
         <section class="section">
             <div id="contentApp" class="container">
+                <div class="columns">
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">Pending Revenue</p>
+                            <div class="card-content">
+                               <p class="title is-3">Rp. {{ Value | currency}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">Pending Invoice</p>
+                            <div class="card-content">
+                               <p class="title is-3">Rp. {{ Value | currency}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">Revenue Achieved</p>
+                            <div class="card-content">
+                               <p class="title is-3">Rp. {{ Value | currency}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">Invoice Achieved</p>
+                            <div class="card-content">
+                               <p class="title is-3">Rp. {{ Value | currency}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">Activations</p>
+                            <div class="card-content" v-if="isValidation">
+                                <activation-chart :activation="activation"></activation-chart>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">BAST</p>
+                            <div class="card-content">
+                                <!-- <activation-chart></activation-chart> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                            <p class="panel-heading">Project Running</p>
+                            <div class="card-content">
+                                <!-- <activation-chart></activation-chart> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <div class="card dashboard-rounded">
+                                <p class="panel-heading">IWO - 2020</p>
+                                <div class="card-content">
+                                    <iwo-chart></iwo-chart>
+                                </div>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </section>
     </section>
 </template>
+
+<script>
+import ActivationChart from '@/components/AM/Dashboard/Chart'
+import axios from 'axios'
+
+export default {
+    components: {
+        ActivationChart,
+    },
+    data() {
+        return {
+            Value:13131331,
+            activation: {},
+            isLoading: true,
+            isValidation: false
+        }
+    },
+    methods: {
+        getActivation() {
+            this.isLoading = true;
+            let self = this;
+            axios
+                .get("http://localhost:3000/Activations")
+                .then(function(response) {
+                    self.activation = response.data
+                    self.isValidation = true
+                })
+                .catch(function(error) {
+                    console.log(error);
+                })
+                .finally(() => (self.isLoading = false));
+        }
+    },
+    mounted() {
+        this.getActivation()
+    }
+}
+</script>
